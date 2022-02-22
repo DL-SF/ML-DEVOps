2e016a3301ddab616168776416e9abb007f297c9
subhankar <91703600+DL-SF@users.noreply.github.com>
2022-02-22 17:52:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from torch.autograd.gradcheck import gradcheck
-
-    def test_sanity(self):
-        input = (Variable(torch.randn(20, 20).double(), requires_grad=True), )
-        model = nn.Linear(20, 1).double()
-        test = gradcheck(model, input, eps=1e-6, atol=1e-4)
-        print(test)
+def test_sanity(self):
+    input = (Variable(torch.randn(20, 20).double(), requires_grad=True), )
+    model = nn.Linear(20, 1).double()
+    test = gradcheck(model, input, eps=1e-6, atol=1e-4)
+    print(test)
